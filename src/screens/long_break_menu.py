a3def8fd9765456c92058b09504035b7ae81f929
NoePfister <noe.pfister@gmx.ch>
2024-07-30 13:11:11 +0200

    finished menus

--- src/screens/long_break_menu.py
+++ src/screens/long_break_menu.py
@@ -9,8 +9,21 @@ class LongBreakMenu(Screen):
             self.print_screen()
             key: bytes = self.wait_for_input()
             match key:
+                case b'1':
+                    self.change_screen("long_break_screen")
+                case b'2':
+                    self.change_screen("menu")
+                case b'3':
+                    self.exit()
                 case _:
                     continue
 
     def print_screen(self) -> None:
-        print("")
+        print("START LONG BREAK \n\n " +
+
+              "__20 minutes__ \n\n " +
+
+              "Options: \n " +
+              "- (1) Start \n " +
+              "- (2) Cancel \n " +
+              "- (3) Quit ")
--- src/screens/short_break_menu.py
+++ src/screens/short_break_menu.py
@@ -9,8 +9,22 @@ class ShortBreakMenu(Screen):
             self.print_screen()
             key: bytes = self.wait_for_input()
             match key:
+                case b'1':
+                    self.change_screen("short_break_screen")
+                case b'2':
+                    self.change_screen("menu")
+                case b'3':
+                    self.exit()
                 case _:
                     continue
 
     def print_screen(self) -> None:
-        print("")
+        print("START SHORT BREAK \n " +
+
+              "__5 minutes__ \n " +
+
+              "Options: \n " +
+              "- (1) Start \n " +
+              "- (2) Cancel \n " +
+              "- (3) Quit"
+              )
